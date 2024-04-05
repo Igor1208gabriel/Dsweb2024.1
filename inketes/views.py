71f96e7f3635695046f9c2834c9b3fc028ba08c3
Igor Santos <santos.igor1@escolar.ifrn.edu.br>
2024-04-05 12:43:44 +0000

    Atualizações do dia 05/04

--- inketes/views.py
+++ inketes/views.py
@@ -1,28 +1,59 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.http      import HttpResponseRedirect
+from django.urls      import reverse
+from .models import Question, Choice
 
 
 def index(request): #parametro: HttpRequest
-    enquetes = Question.objects.order_by('pub_date')[:10]
+    enquetes = Question.objects.order_by('id')[:10]
     contexto = {'lista_enquetes': enquetes}
     return render(request, 'inketes/index.html', contexto)
 
 
 def detalhes(request, pergunta_id):
-    que = get_object_or_404(Question, pk = pergunta_id)
+    que      = get_object_or_404(Question, pk = pergunta_id)
     contexto = {"inkete": que}
     return render(request, 'inketes/detalhes.html', contexto)
 
 
 def votacao(request, pergunta_id):
-    resultado = "VOTAÇÃO da inkete de número %s"
-    return HttpResponse(resultado % pergunta_id)
+    que = get_object_or_404(Question, pk=pergunta_id)
+    try:
+        idalt = request.POST['choice']
+        alt   = que.choice_set.get(pk=idalt)
+    except (KeyError, Choice.DoesNotExist):
+        contexto = {
+            'inkete': que,
+            'error': 'Você precisa selecionar uma alternativa'
+            }
+        return render(request, 'inketes/detalhes.html', contexto)
+    else:
+        alt.votes += 1
+        alt.save()
+        return HttpResponseRedirect(reverse(
+            'inketes:resultado', args=(pergunta_id,)
+            ))
+
+
 
 def resultado(request, pergunta_id):
-    resultado = 'RESULTADO da inkete de número %s'
-    return HttpResponse(resultado % pergunta_id)
+    que = get_object_or_404(Question, pk = pergunta_id)
+    return render(request, 'inketes/resultado.html', {"inkete": que} )
 
 
 
-#mudança pra commitar
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
